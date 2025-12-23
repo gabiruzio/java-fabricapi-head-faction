0955f408ebe2af7ec4dc8e12ed92ec02028b1b34
RogerPurple <rogerroxofraga5@gmail.com>
2025-12-23 10:14:57 -0300

    Starting creation off CustomRender

---
+++ src/client/java/com/battlefactions/mixin/GameRendererMixin.java
@@ -0,0 +1,19 @@
+package com.battlefactions.mixin;
+
+import com.battlefactions.render.CustomRenderPipeline;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.renderer.GameRenderer;
+
+
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+    @Inject(method = "close", at = @At("RETURN"))
+    private void onGameRendererClose(CallbackInfo ci) {
+        CustomRenderPipeline.getInstance().close();
+    }
+}
--- src/main/java/com/battlefactions/blocks/custom/VictorySymbolBlock.java
+++ src/main/java/com/battlefactions/blocks/custom/VictorySymbolBlock.java
@@ -7,6 +7,7 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -19,10 +20,13 @@ public class VictorySymbolBlock extends Block {
     public static final EnumProperty<Direction> FACING;
     private static final VoxelShape SHAPE;
 
+    public static final BooleanProperty XRAY = BooleanProperty.create("xray");
+
 
     public VictorySymbolBlock(Properties properties) {
         super(properties);
         this.registerDefaultState((BlockState)((BlockState)this.stateDefinition.any()).setValue(FACING, Direction.NORTH));
+        this.registerDefaultState((BlockState) ((BlockState)this.stateDefinition.any()).setValue(XRAY, false));
     }
 
     @Override
@@ -43,6 +47,7 @@ public class VictorySymbolBlock extends Block {
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(new Property[]{FACING});
+        builder.add(new Property[]{XRAY});
     }
 
     static {
